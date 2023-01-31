5f900afa08a4e9220e5868d1c1529f3d4351fefa
stridger <stridger@opencodes.org>
2023-01-31 09:45:40 +0000

    Enable OTA updates

--- Sofar2mqtt/Sofar2mqtt.ino
+++ Sofar2mqtt/Sofar2mqtt.ino
@@ -89,6 +89,9 @@ calcCRC by angelo.compagnucci@gmail.com and jpmzometa@gmail.com
 
 #include <Arduino.h>
 
+// OTA
+#include <ArduinoOTA.h>
+
 #define SOFAR_SLAVE_ID          0x01
 
 #ifdef INVERTER_ME3000
@@ -349,6 +352,40 @@ void setup_wifi()
 	}
 
 	WiFi.hostname(deviceName);
+
+	ArduinoOTA.onStart([]() {
+		String type;
+		if (ArduinoOTA.getCommand() == U_FLASH) {
+		type = "sketch";
+		} else { // U_FS
+		type = "filesystem";
+		}
+
+		// NOTE: if updating FS this would be the place to unmount FS using FS.end()
+		Serial.println("Start updating " + type);
+	});
+	ArduinoOTA.onEnd([]() {
+		Serial.println("\nEnd");
+	});
+	ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+		Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+	});
+	ArduinoOTA.onError([](ota_error_t error) {
+		Serial.printf("Error[%u]: ", error);
+		if (error == OTA_AUTH_ERROR) {
+		Serial.println("Auth Failed");
+		} else if (error == OTA_BEGIN_ERROR) {
+		Serial.println("Begin Failed");
+		} else if (error == OTA_CONNECT_ERROR) {
+		Serial.println("Connect Failed");
+		} else if (error == OTA_RECEIVE_ERROR) {
+		Serial.println("Receive Failed");
+		} else if (error == OTA_END_ERROR) {
+		Serial.println("End Failed");
+		}
+	});
+	ArduinoOTA.begin();
+
 	Serial.println("");
 	Serial.print("WiFi connected - ESP IP address: ");
 	Serial.println(WiFi.localIP());
@@ -901,6 +938,9 @@ void loop()
 
 	//Set battery save state
 	batterySave();
+
+	// OTA handle
+	ArduinoOTA.handle();	
 
 	delay(100);
 }
